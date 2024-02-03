c65fde74395fa9a0699da2fdc04c57780e7721ae
Souzaa <122699226+zinfex@users.noreply.github.com>
2024-02-03 19:07:12 -0300

    Alterações na página

--- Link-in-Bio/speed-links-react/src/App.jsx
+++ Link-in-Bio/speed-links-react/src/App.jsx
@@ -11,25 +11,17 @@ function App() {
 
   return (
     <>
-      {/* <img src="../src/imgs/nuvem/Nuvem-02.png" className="nuvem nuvem2"/>
-      <img src="../src/imgs/nuvem/Nuvem-03.png" className="nuvem nuvem3"/> */}
-    
       <div class="conteinerMain">
         <div class="conteinerHead">
           <img src={logo} className="imgPerfil" alt="Img logo" />
-          {/* <img src='' className="nuvem nuvem1"/> */}
         </div>
 
         <div class="conteinerBody">
-            <h3>Links SpeedCloud</h3>
+            <h3>Para cada demanda, a solução ideal na nuvem</h3>
           
-            <a href="https://www.speedcloud.com.br/" className="button" target='_blank'>Site incrível</a>
+            <a href="https://www.speedcloud.com.br/" className="button" target='_blank'>Nosso Site</a>
           
-            <a href="https://api.whatsapp.com/send?phone=558530236062&text=Ol%C3%A1" className="button" target='_blank'>Tire suas dúvidas!</a>
-        
-            <a href="https://www.linkedin.com/company/101826655/admin/feed/posts/?feedType=following" className="button" target='_blank' >LinkedIn</a>
-        
-            <a href="https://www.youtube.com/@HostIDC/featured" className="button" target='_blank'>Youtube</a>
+            <a href="https://api.whatsapp.com/send?phone=558530236062&text=Ol%C3%A1" className="button" target='_blank'>Chama no Zap</a>
         
         </div>
           <h4>Redes Sociais</h4>
@@ -39,7 +31,7 @@ function App() {
               <a href="https://www.facebook.com/speedcloudbr/?paipv=0&eav=AfZi47P5U5jpueFlG4yusKxL_qV5zYDqIv9U_RUkWK2ITfjyXvMUd-MaDDjAhPsk9mk&_rdr#_=_" target='_blank'>
               <FaFacebookF />
               </a>
-              <a href="https:j//www.instagram.com/speedcloud_/" target='_blank'>
+              <a href="https://www.instagram.com/speedcloud_/" target='_blank'>
               <FaInstagram />
               </a>
               <a href="https://api.whatsapp.com/send?phone=558530236062&text=Ol%C3%A1" target='_blank'>
@@ -49,7 +41,7 @@ function App() {
               <FaLinkedinIn />
               </a>
           </div> 
-          <p> Copyright © 2024. 085.digital | Todos os direitos reservados. <MdVerifiedUser /></p>
+          <p> Copyright © 2024. SpeedCloud | Todos os direitos reservados. <MdVerifiedUser /></p>
           </footer>
         </div>
       </div>
